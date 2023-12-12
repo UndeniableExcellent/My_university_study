2d25c9a12f3380609c3773cb3aa5c487da2ce824
UndeniableExcellent <1457336271@qq.com>
2023-12-12 22:09:11 +0800

    commit

--- mooc/unit7.c
+++ mooc/unit7.c
@@ -3,18 +3,18 @@
 // {
 //     int a[5]={ 1,2 ,3,4,5},*p=&a; *p=5;
 //     printf("a[0]=%d\n",a[0]);
-//     // int a,*p=&a;*p=2; 
+//     // int a,*p=&a;*p=2;
 //     // printf("%d\n",a);
 //     return 0;
 // }
 
-// int main()
-// {
-//     double arr[10],(*p)[10]=&arr;
-//     scanf("%lf",p[6]);
-//     printf("%lf\n",arr[6]);
-//     return 0;
-// }
+int main()
+{
+    double arr[10],*p=arr;
+    scanf("%lf",p+6);
+    printf("%lf\n",arr[6]);
+    return 0;
+}
 // int main()
 // {
 //     // int a[10]={0,1,2,3,4,5,6,7,8,9},*p; p=a+1;
@@ -39,16 +39,46 @@
 //     return 0;
 // }
 
-void fun(int x,int *y)
-{
-     x+=*y;
-    *y+=x;
-}
-int main( )
-{
-    int x=5,y=10;
-    fun(x,&y);
-    fun(y,&x);
-     printf("%d,%d",x,y);
-     return 0;
-}
+// void fun(int x,int *y)
+// {
+//      x+=*y;
+//     *y+=x;
+// }
+// int main( )
+// {
+//     int x=5,y=10;
+//     fun(x,&y);
+//     fun(y,&x);
+//      printf("%d,%d",x,y);
+//      return 0;
+// }
+
+// int main( )
+// {
+//   int arr[10]={2,3,-9,5,7,0,4,-1,6,-7},*p;
+//   int sum=0;
+//   for (p=&arr[3];p<arr+10;)
+//     sum+=*p++;
+//   printf("%d",sum);
+//   return 0;
+// }
+
+// #define N 5
+// int fun(int *s, int a, int n)
+// {
+//     int j;
+//     *s = a;
+//     j = n;
+//     while (a != s[j])
+//         j--;
+//     return j;
+// }
+// int main()
+// {
+//     int s[N + 1];
+//     int k;
+//     for (k = 1; k <= N; k++)
+//         s[k] = k + 1;
+//     printf("%d\n", fun(s, 4, N));
+//     return 0;
+// }
