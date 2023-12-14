001ed0fa60726980e4dbbf96192ab69f04e3f466
UndeniableExcellent <1457336271@qq.com>
2023-12-14 10:49:36 +0800

    exp6_2

--- B23051217/exp3/exp6_2.c
+++ B23051217/exp3/exp6_2.c
@@ -26,3 +26,55 @@ a=b;
 b=temp;
  }
 */
+
+#include <stdio.h>
+void swap1(int a, int b);
+void swap2(int *a, int b);
+void swap3(int *a, int *b);
+void swap4(int *a, int *b);
+
+int main(void)
+{
+	int x = 1, y = 2;
+	printf("x=%d,y=%d\n", x, y);
+	swap1(x, y);
+	printf("x=%d,y=%d\n", x, y);
+    x = 1, y = 2;
+	swap2(&x, y);
+	printf("x=%d,y=%d\n", x, y);
+    x = 1, y = 2;
+	swap3(&x, &y);
+	printf("x=%d,y=%d\n", x, y);
+    x = 1, y = 2;
+	swap4(&x, &y);
+	printf("x=%d,y=%d\n", x, y);
+	return 0;
+}
+
+void swap1(int a, int b)
+{
+	int temp= a;
+	a = b;
+	b = temp;
+}
+
+void swap2(int *a, int b)
+{
+	int temp= *a;
+	*a = b;
+	b = temp;
+}
+
+void swap3(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void swap4(int *a, int *b)
+{
+	int *temp = a;
+	a=b;
+	b=temp;
+}
