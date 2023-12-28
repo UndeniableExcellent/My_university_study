cc55d67c33616b01ab3a7ec6f61f9fc3b562ebc8
UndeniableExcellent <1457336271@qq.com>
2023-12-28 19:04:17 +0800

    文本文件读写

--- mooc/unit11.c
+++ mooc/unit11.c
@@ -45,15 +45,34 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+// int main()
+// {
+//     FILE *pf;
+//     char *s1 = "China", *s2 = "Beijing";
+//     pf = fopen("abc.dat", "wb+");
+//     fwrite(s2, 7, 1, pf);
+//     rewind(pf);
+//     fwrite(s1, 5, 1, pf);
+//     fclose(pf);
+//     return 0;
+// }
+
 #include <stdio.h>
 int main()
 {
-    FILE *pf;
-    char *s1 = "China", *s2 = "Beijing";
-    pf = fopen("abc.dat", "wb+");
-    fwrite(s2, 7, 1, pf);
-    rewind(pf);
-    fwrite(s1, 5, 1, pf);
-    fclose(pf);
+    FILE *fp;
+    int k, n, i, a[6] = {10, 20, 30, 40, 50, 60};
+    fp = fopen("d2.txt", "w");
+    for (i = 0; i < 6; i++)
+        fprintf(fp, "%d,%d\n", i, a[i]);
+    fclose(fp);
+    fp = fopen("d2.txt", "r");
+    for (i = 0; i < 3; i++)
+    {
+        fscanf(fp, "%d,%d", &k, &n);
+        printf("%d,%d\n", k, n);
+    }
+    fclose(fp);
     return 0;
 }
