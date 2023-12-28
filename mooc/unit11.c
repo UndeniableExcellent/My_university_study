4bc360298e06660b96b3d0501adcf38e4a83f95c
UndeniableExcellent <1457336271@qq.com>
2023-12-28 18:56:54 +0800

    文本文件操作

--- mooc/unit11.c
+++ mooc/unit11.c
@@ -1,16 +1,46 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main()
+// {
+//     FILE *fp;
+//     fp = fopen("abc.txt", "r+");
+//     while (!feof(fp))
+//         if (fgetc(fp) == '*')
+//         {
+//             fseek(fp, -1, 1);
+//             fputc('$', fp);
+//             fflush(fp);
+//         }
+//     fclose(fp);
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
 int main()
 {
+    int i, n;
     FILE *fp;
-    fp = fopen("abc.txt", "r+");
-    while (!feof(fp))
-        if (fgetc(fp) == '*')
-        {
-            fseek(fp, -1, 1);
-            fputc('$', fp);
-            fflush(fp);
-        }
+    if ((fp = fopen("temp", "w+")) == NULL)
+    {
+        printf("不能建立temp文件\n");
+        exit(0);
+    }
+    for (i = 1; i <= 10; i++)
+        fprintf(fp, "%3d", i);
+    for (i = 0; i < 10; i++)
+    {
+        fseek(fp, i * 3L, SEEK_SET);
+        fscanf(fp, "%d", &n);
+        fseek(fp, i * 3L, 0);
+        fprintf(fp, "%3d", n + 10);
+    }
+    for (i = 0; i < 5; i++)
+    {
+        fseek(fp, i * 6L, 0);
+        fscanf(fp, "%d", &n);
+        printf("%d,", n);
+    }
     fclose(fp);
     return 0;
 }
